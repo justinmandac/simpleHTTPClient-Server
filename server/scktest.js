b1ecd4a82fb90e97bc2ffbeb399d24e077547f41
Bien Justin Mandac <justinmandac@gmail.com>
2014-07-22 20:18:42 +0800

    Used synchronous file reading.

--- server/scktest.js
+++ server/scktest.js
@@ -6,13 +6,27 @@ var listenPort = 9000;
 
 //creates a server instance listening at listenPort
 server.createServer(function (socket){
-	
 	socket.on('data', function(data){
 	//even handler when data is received from client
-		response = 'dong';
 		splitHeader = data.toString().split(' ');
 		console.log(splitHeader);
-		socket.write('<html></html>'); //replace with data
+		//check what kind of request is made
+		if(splitHeader[0] == 'GET')
+		{
+			//acquire file specified in the resource path (splitHeader[1])
+			file = '';
+			filename = '';
+			if (splitHeader[1] == '/')
+			{
+				filename = './index.html';
+			}
+			else
+			{
+				filename = '.'+splitHeader[1];
+			}
+			file = fs.readFileSync(filename);
+			socket.write(file.toString()); //replace with data
+		}
 	});
 	
 }).listen(listenPort, function(){
